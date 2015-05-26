3a6962a5f195899b2a6fbfcae5f603ed8c238957
Dean Shi <dean.xiaoshi@gmail.com>
2015-05-26 11:26:09 -0700

    added autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var ejs = require('gulp-ejs');
 var stylus = require('gulp-stylus');
 var minifyHTML = require('gulp-minify-html');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Compile EJS
 gulp.task('ejs', function () {
@@ -19,6 +20,9 @@ gulp.task('stylus', function () {
     return gulp.src('./main.styl')
         .pipe(stylus({
             compress: true
+        }))
+        .pipe(autoprefixer({
+            cascade: false
         }))
         .pipe(gulp.dest('./dist'));
 });
