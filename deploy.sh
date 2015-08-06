f63defcb8c470e01f21478874124fdecf495931f
Dean Shi <dean.xiaoshi@gmail.com>
2015-08-06 14:16:35 -0700

    Update travis.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,11 @@ ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
 cd dist
+git init
+
+git config --global user.name "Travis CI"
+git config --global user.email "dean.xiaoshi@gmail.com"
+
 git add .
 git commit -m "Deployed to Github Pages $(date +'%Y-%m-%d %T')"
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
