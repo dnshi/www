36152b51ac04b7696ffa8a2f6fb706df99b2d03e
Dean Shi <dean.xiaoshi@gmail.com>
2015-08-06 14:32:58 -0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ set -e # exit with nonzero exit code if anything fails
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
-
 cd dist
 git init
 
@@ -15,6 +12,6 @@ git config --global user.email "dean.xiaoshi@gmail.com"
 
 git add .
 git commit -m "Deployed to Github Pages $(date +'%Y-%m-%d %T')"
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
 echo "Deployed successfully."
