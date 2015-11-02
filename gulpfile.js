e745bfc7d4e40c9c75d2c738dbf286545f15732a
Dean Shi <dean.xiaoshi@gmail.com>
2015-11-01 19:58:31 -0800

    Add description for education

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,27 @@ fs = require('fs'),
 config = require('./config.json');
 
 // Compile EJS
-gulp.task('ejs', ['clean', 'stylus'], function () {
-    fs.readFile('./dist/static/main.css', 'utf8', function (err, data) {
-        return err ? console.log(err) : config.css = data;
-    });
+gulp.task('ejs', ['clean', 'stylus'], () => {
+  fs.readFile('./dist/static/main.css', 'utf8', (err, data) =>
+    err ? console.log(err) : config.css = data
+  );
 
-    return gulp.src('./index.ejs')
-        .pipe(ejs(config))
-        .pipe(minifyHTML({ conditionals: true, spare: true }))
-        .pipe(gulp.dest('./dist'));
+  return gulp.src('./index.ejs')
+    .pipe(ejs(config))
+    .pipe(minifyHTML({ conditionals: true, spare: true }))
+    .pipe(gulp.dest('./dist'));
 });
 
 // Compile CSS from stylus files
-gulp.task('stylus', ['clean'], function () {
-    return gulp.src('./static/main.styl')
-        .pipe(stylus({ compress: true }))
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(gulp.dest('./dist/static'));
-});
-
-// Copy png files to dist
-gulp.task('copy', ['clean'], function () {
-    return gulp.src('./static/*.png')
-        .pipe(gulp.dest('./dist/static'));
-});
+gulp.task('stylus', ['clean'], () =>
+  gulp.src('./static/main.styl')
+    .pipe(stylus({ compress: true }))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest('./dist/static'))
+);
 
 // Clean dist
-gulp.task('clean', function (cb) {
-    return del('./dist', cb);
-});
+gulp.task('clean', (cb) => del('./dist', cb));
 
 // Build
-gulp.task('default', ['ejs', 'copy']);
+gulp.task('default', ['ejs']);
