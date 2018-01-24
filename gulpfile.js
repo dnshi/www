70c54f315f1cc914df7aa30ed2fe1e7e288fb6d4
Dean Shi <dean.xiaoshi@gmail.com>
2018-01-24 14:39:54 -0800

    Improve code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,23 @@
-const
-gulp = require('gulp'),
-ejs = require('gulp-ejs'),
-stylus = require('gulp-stylus'),
-htmlmin = require('gulp-htmlmin'),
-autoprefixer = require('gulp-autoprefixer'),
-del = require('del'),
-fs = require('fs'),
-config = require('./src/config.json');
+const gulp = require('gulp')
+const ejs = require('gulp-ejs')
+const stylus = require('gulp-stylus')
+const htmlmin = require('gulp-htmlmin')
+const autoprefixer = require('gulp-autoprefixer')
+const del = require('del')
+const { readFile } = require('fs')
+const { promisify } = require('util')
+const config = require('./src/config.json')
+
+const readFileAsync = promisify(readFile)
 
 // Compile EJS
-gulp.task('ejs', ['clean', 'stylus'], () => {
-  fs.readFile('./dist/static/main.css', 'utf8', (err, data) =>
-    err ? console.log(err) : config.css = data
-  );
-
+gulp.task('ejs', ['clean', 'stylus'], async () => {
+  config.css = await readFileAsync('./dist/static/main.css', 'utf8')
   return gulp.src('./src/index.ejs')
     .pipe(ejs(config, {}, { ext: '.html' }))
-    .pipe(htmlmin({removeComments: true, collapseWhitespace: true, removeAttributeQuotes: true}))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(htmlmin({ removeComments: true, collapseWhitespace: true, removeAttributeQuotes: true }))
+    .pipe(gulp.dest('./dist'))
+})
 
 // Compile stylus to css file
 gulp.task('stylus', ['clean'], () =>
@@ -26,10 +25,10 @@ gulp.task('stylus', ['clean'], () =>
     .pipe(stylus({ compress: true }))
     .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest('./dist/static'))
-);
+)
 
 // Clean dist
-gulp.task('clean', (cb) => del('./dist', cb));
+gulp.task('clean', (cb) => del('./dist', cb))
 
 // Build
-gulp.task('default', ['ejs']);
+gulp.task('default', ['ejs'])
