1f639485d6642a95109c797ae065251ea5bdec12
Dean Shi <dean.xiaoshi@gmail.com>
2019-07-15 15:59:34 -0700

    Remove travis, delete deploy.sh and uninstall del package

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ const ejs = require('gulp-ejs')
 const stylus = require('gulp-stylus')
 const htmlmin = require('gulp-htmlmin')
 const autoprefixer = require('gulp-autoprefixer')
-const del = require('del')
-const { readFile } = require('fs')
-const { promisify } = require('util')
+const {
+  readFile
+} = require('fs')
+const {
+  promisify
+} = require('util')
 const config = require('./src/config.json')
 
 const readFileAsync = promisify(readFile)
@@ -14,29 +17,35 @@ const readFileAsync = promisify(readFile)
 async function runEJS() {
   config.css = await readFileAsync('./dist/static/main.css', 'utf8')
   return gulp.src('./src/index.ejs')
-    .pipe(ejs(config, {}, { ext: '.html' }))
-    .pipe(htmlmin({ removeComments: true, collapseWhitespace: true, removeAttributeQuotes: true }))
+    .pipe(ejs(config, {}, {
+      ext: '.html'
+    }))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true
+    }))
     .pipe(gulp.dest('./dist'))
 }
 
 // Compile stylus to css file
 function runStylus() {
   return gulp.src('./src/main.styl')
-    .pipe(stylus({ compress: true }))
-    .pipe(autoprefixer({ cascade: false }))
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(gulp.dest('./dist/static'))
 }
 
-// Clean dist
-const clean = () => del('./dist')
-
 // Export modules
-exports.clean = clean;
 exports.runStylus = runStylus;
 exports.runEJS = runEJS;
 
 // Run task
-const build = gulp.series(clean, runStylus, runEJS);
+const build = gulp.series(runStylus, runEJS);
 gulp.task('build', build);
 
 // Build
